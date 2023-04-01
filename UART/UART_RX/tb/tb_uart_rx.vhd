07c1b944f9e2014cd1d8615e38556314af5eb693
Abel Apera DIDOUH <122778382+abel-did@users.noreply.github.com>
2023-04-01 15:40:54 +0200

    tb_uart_rx.vhd

--- UART/UART_RX/tb/tb_uart_rx.vhd
+++ UART/UART_RX/tb/tb_uart_rx.vhd
@@ -21,14 +21,63 @@ architecture testbench of tb_uart_tx is
   
   signal clk    : std_logic:= '0';
   signal resetn : std_logic;
+  signal busy   : std_logic;
+  signal rx     : std_logic;
+  signal data_out : std_logic_vector(N-1 downto 0);
   
 	
 begin
   clk <= not clk after hp;
   
-  stimuli : process is
-  begin
-    
+ 
+stimuli : process is
+begin
+  resetn <= '0';
+  
+  --rx <= '1'; -- Bit 7
+  --wait for 1275 ns;
+  --resetn <= '1';
+  --rx <= '1'; -- Bit 6
+  --wait for 500 ns;
+  --rx <= '0'; -- Bit 5
+  --wait for 500 ns;
+  --rx <= '1'; -- Bit 4
+  --wait for 500 ns;
+  --rx <= '0'; -- Bit 3
+  --wait for 500 ns;
+  --rx <= '1'; -- Bit 2
+  --wait for 500 ns;
+  --rx <= '0'; -- Bit 1
+  --wait for 500 ns;
+  --rx <= '1'; -- Bit 0
+  --wait for 265 ns;
+
+		-- A 01000001
+  
+  rx <= '1';
+  
+  wait for 1000 ns;
+  resetn <= '1';
+  rx <= '1'; -- Bit Repos
+  wait for 500 ns;
+  rx <= '1'; -- Bit Repos
+  wait for 500 ns;
+  rx <= '0'; -- Bit 7
+  wait for 500 ns;
+  rx <= '1'; -- Bit 6
+  wait for 500 ns;
+  rx <= '0'; -- Bit 5
+  wait for 500 ns;
+  rx <= '0'; -- Bit 4
+  wait for 500 ns;
+  rx <= '0'; -- Bit 3
+  wait for 500 ns;
+  rx <= '0'; -- Bit 2
+  wait for 500 ns;
+  rx <= '0'; -- Bit 1
+  wait for 500 ns;
+  rx <= '1'; -- Bit 0
+  wait for 265 ns;
 
 
   wait;
@@ -43,6 +92,8 @@ begin
   port map (
     clk    => clk   ,
     resetn => resetn,
-    
+    data_out   => data_out  ,
+    busy  => busy ,
+    rx     => rx 
   );
 end architecture;
